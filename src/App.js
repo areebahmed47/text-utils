28b0aeb956579d0635be9e4491b718366e9f1360
Areeb Ahmed <areebahmed1018@gmail.com>
2024-08-08 22:33:18 +0500

    add basename in app.js
    add basename in app.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   };
   return (
     <>
-       <Router>
+       <Router basename="/text-utils">
       <NavBar
         title="TextUtils"
         aboutText="About"
@@ -45,8 +45,8 @@ function App() {
       <Alert alert={alert} />
       <div className="container my-3">
         <Routes>
+          <Route exact path="/text-utils" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
           <Route exact path="/about" element={<About mode = {mode} />} />
-          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
         </Routes>
       </div>
     </Router>
